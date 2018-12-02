599060d3a928268c4f8e3ef1999ccb6444887134
Cassius Vonthill <39351381+CassiusVonthill@users.noreply.github.com>
2018-12-02 13:28:19 +0000

    PR: Add my Part 1 to Repo (#1) (#1)
    * Init + D1P1
    
    * D1P2 added
    
    * Trying this again
    
    * Uploading p1
    
    * Moved Files into folder

---
+++ Trian-Java/D1P1.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+class D1P1 {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+            int lines = 0;
+            int total = 0;
+            do {
+                String line = reader.readLine();
+                String operand = line.replaceAll("[0-9]*$", "");
+                String value = line.replaceAll("[-+*/]", "");
+                if(operand.equals("+")){
+                    total += Integer.parseInt(value);
+                }
+                if(operand.equals("-")){
+                    total -= Integer.parseInt(value);
+                }
+                lines++;
+            } while (lines < 967);
+            System.out.println(total);
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
