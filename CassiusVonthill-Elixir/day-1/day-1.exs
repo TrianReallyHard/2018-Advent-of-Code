fc4d4cda48aa78741f177f1108a5ca64d17d4755
Cassius Vonthill <39351381+CassiusVonthill@users.noreply.github.com>
2018-12-02 13:30:05 +0000

    CassiusVonthill Day 1 Part 2 (#2)
    * Day 1: Part 2 Complete
    
    * :memo: Elixir README
    
    * PR: Add my Part 1 to Repo (#1) (#1)
    
    * Init + D1P1
    
    * D1P2 added
    
    * Trying this again
    
    * Uploading p1
    
    * Moved Files into folder

--- CassiusVonthill-Elixir/day-1/day-1.exs
+++ CassiusVonthill-Elixir/day-1/day-1.exs
@@ -1,8 +1,46 @@
 defmodule Day1 do
+
   def part1(string_input) do
+    parse_input(string_input)
+    |> Enum.sum()
+  end
+
+  def part2(string_input) do
+    parse_input(string_input)
+    # |> IO.inspect(label: "Parsed Data")
+    |> Stream.cycle()
+    # |> IO.inspect(label: "Cycle data")
+    |> set_handler()
+    |> elem(0)
+  end
+
+  def parse_input(string_input) do
     Regex.scan(~r/[+-]\d+/, string_input)
     |> List.flatten()
-    |> IO.inspect(label: "Flattened data")
-    |> Enum.reduce(0, fn num, acc -> String.to_integer(num) + acc end)
+    # |> IO.inspect(label: "Flattened data")
+    |> Enum.map(&String.to_integer(&1))
+    # |> IO.inspect(label: "Numerical data")
   end
+
+  def set_handler(s) do # Rather slow and inefficient. Lots of looping
+
+    Enum.reduce_while(s, {0, MapSet.new([0])}, fn x, {base, ms} -> 
+      base =
+        base
+        |> Kernel.+(x)
+        |> IO.inspect(label: "Base")
+
+      if MapSet.member?(ms, base) do
+        # IO.puts("true")
+        {:halt, {base, ms}}
+      else
+        # IO.puts("false")
+        ms = 
+          MapSet.put(ms, base)
+          # |> IO.inspect()
+        {:cont, {base, ms}}
+      end
+    end)
+  end
+
 end
