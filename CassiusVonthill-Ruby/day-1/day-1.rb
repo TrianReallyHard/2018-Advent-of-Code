09547ad355acf1389acb8e1f6f6a0b8b490794a8
Cassius Vonthill <cassiusvonthill@gmail.com>
2018-12-08 23:43:23 -0500

    Ruby Day 1 Part 2

--- CassiusVonthill-Ruby/day-1/day-1.rb
+++ CassiusVonthill-Ruby/day-1/day-1.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 class Day1
 
   def parse_input(input)
@@ -6,6 +8,19 @@ class Day1
 
   def part_1(input)
     return parse_input(input).reduce(0, :+)
+  end
+
+  def part_2(input)
+    base = 0
+    cache = Set.new [0]
+    parse_input(input).cycle do |x|
+      base += x
+      if cache.include? base
+        return base
+      else
+        cache.add(base)
+      end
+    end
   end
 
 end
