9782d4a523153fb527c2507db8d9d198eabcc79c
Ian Smith <Trian@Ians-MBP-2.fios-router.home>
2018-12-09 23:17:26 -0500

    Part 2 done! Day 3 up next.

--- Trian-Java/D2P2.java
+++ Trian-Java/D2P2.java
@@ -9,11 +9,11 @@ import java.util.ArrayList;
 
 class D2P2 {
 
-    public static List<Map<Character, Integer>> charMapList = new ArrayList<Map<Character, Integer>>();
+    public static List<String> codeList = new ArrayList<String>();
 
     public static void main(String[] args) {
         verify();
-        findCodes(charMapList);
+        findCodes(codeList);
     }
 
     public static void verify(){
@@ -35,7 +35,7 @@ class D2P2 {
                         charCount.put(charFound, charCount.get(charFound) + 1);
                     }
                 }
-                charMapList.add(charCount);
+                codeList.add(line);
             }
             scan.close();
         } catch (Exception e) {
@@ -43,19 +43,38 @@ class D2P2 {
         }
     }
 
-    public static void findCodes(List<Map<Character, Integer>> list) {
-        System.out.println(list.size());
-        Stack<Integer> badMaps = new Stack<Integer>();
-        Map<Character, Integer> first = list.get(0);
-        for (Map<Character, Integer> map : list) {
-            if(map.size() == first.size()) {
-                badMaps.push(list.indexOf(map));
+    public static void findCodes(List<String> list) {
+        int mistakesAllowed;
+        String solution = null;
+        for (int i = 0; i < list.size(); i++) {
+            String word1 = list.get(i);
+            for (int j = i + 1; j < list.size(); j++) {
+                mistakesAllowed = 1;
+                String word2 = list.get(j);
+                if(word1.length() == word2.length()) { 
+                    for(int k = 0; k < word1.length(); k++) { 
+                        if(word1.charAt(k) != word2.charAt(k)) { 
+                            mistakesAllowed--; 
+                            if(mistakesAllowed < 0) { 
+                                break;
+                            }
+                        }
+                    }
+
+                    if (mistakesAllowed == 0) {
+                        System.out.println("Success.");
+                        System.out.println("Code 1: " + word1);
+                        System.out.println("Code 2: " + word2);
+                        for(int l = 0; l < word1.length(); l++) { 
+                            if(word1.charAt(l) != word2.charAt(l)) { 
+                                System.out.println("Replace this character: " + word1.charAt(l) + "!");
+                            }
+                        }
+                        solution = word1.replace('g', ' ');
+                        System.out.println("Solution (Delete whitespace): " + solution);
+                    }
+                }
             }
         }
-        while(!badMaps.empty()) {
-            int index = badMaps.pop();
-            list.remove(index);
-        }
-        System.out.println(list.size());
     }
 }
