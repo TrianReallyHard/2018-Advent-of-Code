44adf8f5571a4139d6e8ed9f3a9c7a12a13b910e
Ian Smith <Trian@Ians-MBP-2.fios-router.home>
2018-12-14 00:04:48 -0500

    Help-A-Coder-Out Foundation has a new applicant...

--- Trian-Java/D3/D3P1.java
+++ Trian-Java/D3/D3P1.java
@@ -1,15 +1,12 @@
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import Point;
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+import java.lang.*;
 
 class D3P1 {
 
-    public static int fHeight = 1000;
-    public static int fWidth = 1000;
-    public static List<Integer[]> claims = new ArrayList<Integer[]>();
-    public static List<Integer> solution = new ArrayList<Integer>();
+    public static ArrayList<Integer[]> claims = new ArrayList<Integer[]>();
+    public static Set<Point> solution = new HashSet<Point>();
 
     public static void main(String[] args) {
         processClaims();
@@ -40,47 +37,26 @@ class D3P1 {
         claims.add(claim);
     }
 
-    public static void findOverlap(List<Integer[]> list) {
+    public static void findOverlap(ArrayList<Integer[]> list) {
+        Long timeStart = System.currentTimeMillis();
         for (int i = 0; i < list.size(); i++) {
-            int id1 = list.get(i)[0];
             int left = list.get(i)[1];
             int top = list.get(i)[2];
             int width = list.get(i)[3];
             int height = list.get(i)[4];
-            int xadd = left + width;
-            int yadd = top + height;
-            Point pl1 = new Point();
-            pl1.setX(left);
-            pl1.setY(top);
-            Point pr1 = new Point();
-            pr1.setX(xadd);
-            pr1.setY(yadd);
+            Rectangle rect1 = new Rectangle(left, top, width, height);
             for (int j = i + 1; j < list.size(); j++) {
-                int id2 = list.get(j)[0];
                 int left2 = list.get(j)[1];
                 int top2 = list.get(j)[2];
                 int width2 = list.get(j)[3];
                 int height2 = list.get(j)[4];
-                int xadd2 = left2 + width2;
-                int yadd2 = top2 + height2;
-                Point pl2 = new Point();
-                pl2.setX(left2);
-                pl2.setY(top2);
-                Point pr2 = new Point();
-                pr2.setX(xadd2);
-                pr2.setY(yadd2);
-                if(pl1.getX() > pr2.getX() || pl2.getX() > pr1.getX()) { 
-                    continue;
+                Rectangle rect2 = new Rectangle(left2, top2, width2, height2);
+                if(rect1.intersects(rect2)) {
+                    Rectangle zone = rect1.union(rect2);
                 }
-                solution.add(id1);
-                solution.add(id2);
             }
         }
-        String result = "";
-        for (int id : solution) {
-            result += id;
-            result += ", ";
-        }
-        System.out.println(result);
+        Long timeEnd = System.currentTimeMillis();
+        Long timeTaken = timeEnd - timeStart;
     }
 }
